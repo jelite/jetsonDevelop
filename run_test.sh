685bf0df972ea52f3268c28c288cd36ddcea3505
jelite <jelite@hanyang.ac.kr>
2024-12-16 12:44:33 +0900

    1216-1244

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,5 @@
-export MASTER_ADDR="master_node_ip"
-export MASTER_PORT="29500"
+export MASTER_ADDR="192.168.100.131"
+export MASTER_PORT=29500
 export RANK=0
 export WORLD_SIZE=4
-export LOCAL_RANK=0
-torchrun --nproc_per_node=1 --nnodes=4 --node_rank=0 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT inference.py
+python test.py
