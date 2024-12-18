5bcb786b915218317ee1d39f2d1b6203b021267b
jelite <jelite@hanyang.ac.kr>
2024-12-18 14:52:12 +0900

    1218-1452

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,14 @@
 export MASTER_ADDR="192.168.100.131"
 export MASTER_PORT=29500
-export RANK=0
-export WORLD_SIZE=4
-python3 test.py
+if [ "$(hostname)" = "soda_master" ]; then
+    export RANK=0
+elif [ "$(hostname)" = "soda1" ]; then
+    export RANK=1
+elif [ "$(hostname)" = "soda2" ]; then
+    export RANK=2
+elif [ "$(hostname)" = "soda3" ]; then
+    export RANK=3
+fi
+echo $RANK
+# export WORLD_SIZE=4
+# python3 test.py
---
+++ t.sh
@@ -0,0 +1,2 @@
+h=$(hostname)
+echo ${h}
