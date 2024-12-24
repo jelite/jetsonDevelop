e1411015949e0a17014a3bee18a4e8a8e45db7b2
jelite <jelite@hanyang.ac.kr>
2024-12-24 04:01:18 +0000

    1301

--- network_check.py
+++ network_check.py
@@ -13,10 +13,6 @@ def run(backend):
     tensor = torch.zeros(1)
     
     # Need to put tensor on a GPU device for nccl backend
-    if backend == 'nccl':
-        device = torch.device("cuda:{}".format(LOCAL_RANK))
-        tensor = tensor.to(device)
-
     if WORLD_RANK == 0:
         for rank_recv in range(1, WORLD_SIZE):
             dist.send(tensor=tensor, dst=rank_recv)
@@ -25,15 +21,13 @@ def run(backend):
         dist.recv(tensor=tensor, src=0)
         print('worker_{} has received data from rank {}\n'.format(WORLD_RANK, 0))
 
-def init_processes(backend):
-    dist.init_process_group(backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
+def init_processes():
+    dist.init_process_group(
+        backend='gloo',
+        init_method=f'tcp://{os.environ["MASTER_ADDR"]}:{os.environ["MASTER_PORT"]}',
+        rank=WORLD_RANK,
+        world_size=WORLD_SIZE)
     run(backend)
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
-    parser.add_argument("--backend", type=str, default="nccl", choices=['nccl', 'gloo'])
-    args = parser.parse_args()
-
-    init_processes(backend=args.backend)
+    init_processes()
