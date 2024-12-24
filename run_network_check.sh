e9137cbcb22d299904dc5a52da0bcac69b893acf
jelite <jelite@hanyang.ac.kr>
2024-12-24 03:58:26 +0000

    1258

--- run_network_check.sh
+++ run_network_check.sh
@@ -3,12 +3,12 @@ export MASTER_PORT=29500
 export WORLD_SIZE=2
 export LOCAL_RANK=0
 if [ "$(hostname)" = "master" ]; then
-    export WORLD_RANK=0
+    export RANK=0
 elif [ "$(hostname)" = "soda1" ]; then
-    export WORLD_RANK=1
+    export RANK=1
 elif [ "$(hostname)" = "soda2" ]; then
-    export WORLD_RANK=2
+    export RANK=2
 elif [ "$(hostname)" = "soda3" ]; then
-    export WORLD_RANK=3
+    export RANK=3
 fi
 python3 network_check.py --local_rank=$LOCAL_RANK --backend=gloo
