9b06d3b8f3ba3764a53de25292711a26a2f9ce96
jelite <jelite@hanyang.ac.kr>
2024-12-24 03:51:39 +0000

    1251

--- network_check.py
+++ network_check.py
@@ -10,7 +10,7 @@ def setup(rank, world_size):
     print("Master node initialized at tcp://192.168.100.131:29500")
     
 def main():
-    setup(rank=rank, world_size=world_size)  # Master node is rank 0
+    setup(rank=os.environ['RANK'], world_size=os.environ['WORLD_SIZE'])  # Master node is rank 0
 
 if __name__ == "__main__":
     main()
