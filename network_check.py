32a8523715ec46c05f3afcbcb0a95e1dc9028bc6
jelite <jelite@hanyang.ac.kr>
2024-12-24 03:57:48 +0000

    1257

--- network_check.py
+++ network_check.py
@@ -1,19 +1,39 @@
-import torch.distributed as dist
 import os
+import argparse
 
-def setup(rank, world_size):
-    dist.init_process_group(
-        backend="gloo",  # Use "nccl" if using GPUs
-        init_method="tcp://192.168.100.131:29500",
-        rank=rank,
-        world_size=world_size,
-    )
-    print("Master node initialized at tcp://192.168.100.131:29500")
+import torch
+import torch.distributed as dist
+
+# Environment variables set by torch.distributed.launch
+LOCAL_RANK = int(os.environ['LOCAL_RANK'])
+WORLD_SIZE = int(os.environ['WORLD_SIZE'])
+WORLD_RANK = int(os.environ['RANK'])
+
+def run(backend):
+    tensor = torch.zeros(1)
     
-def main():
-    setup(rank=int(os.environ['RANK']), world_size=int(os.environ['WORLD_SIZE']))  # Master node is rank 0
+    # Need to put tensor on a GPU device for nccl backend
+    if backend == 'nccl':
+        device = torch.device("cuda:{}".format(LOCAL_RANK))
+        tensor = tensor.to(device)
+
+    if WORLD_RANK == 0:
+        for rank_recv in range(1, WORLD_SIZE):
+            dist.send(tensor=tensor, dst=rank_recv)
+            print('worker_{} sent data to Rank {}\n'.format(0, rank_recv))
+    else:
+        dist.recv(tensor=tensor, src=0)
+        print('worker_{} has received data from rank {}\n'.format(WORLD_RANK, 0))
+
+def init_processes(backend):
+    dist.init_process_group(backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
+    run(backend)
 
 if __name__ == "__main__":
-    main()
 
-# nc -zv 192.168.100.131 29500
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
+    parser.add_argument("--backend", type=str, default="nccl", choices=['nccl', 'gloo'])
+    args = parser.parse_args()
+
+    init_processes(backend=args.backend)
--- run_network_check.sh
+++ run_network_check.sh
@@ -1,13 +1,14 @@
 export MASTER_ADDR="192.168.100.131"
 export MASTER_PORT=29500
 export WORLD_SIZE=2
+export LOCAL_RANK=0
 if [ "$(hostname)" = "master" ]; then
-    export RANK=0
+    export WORLD_RANK=0
 elif [ "$(hostname)" = "soda1" ]; then
-    export RANK=1
+    export WORLD_RANK=1
 elif [ "$(hostname)" = "soda2" ]; then
-    export RANK=2
+    export WORLD_RANK=2
 elif [ "$(hostname)" = "soda3" ]; then
-    export RANK=3
+    export WORLD_RANK=3
 fi
-python3 network_check.py
+python3 network_check.py --local_rank=$LOCAL_RANK --backend=gloo
